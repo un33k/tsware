d450025b50b3771d7ca8a8d416d6972e8a34f4f7
Val Neekman <val@neekware.com>
2015-07-04 19:22:06 -0400

    separator in setup file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo -e "Setting up your environment. Be patient!\n"
-echo -e "========================================\n"
+echo -e "Setting up your environment. Be patient!"
+echo -e "========================================"
 npm install
 bower install
 tsd reinstall -os
