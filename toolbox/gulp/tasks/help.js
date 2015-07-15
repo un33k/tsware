45ac402486baa1fb0cb6dc9d71b28504b9ad5666
Val Neekman <val@neekware.com>
2015-07-15 19:33:04 -0400

    add help

---
+++ toolbox/gulp/tasks/help.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var gulp = require('gulp');
+var debug = require('gulp-debug');
+var help = require('gulp-help');
+
+help(gulp);
---
+++ toolbox/gulp/tasks/install.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var debug = require('gulp-debug');
+var run = require('gulp-run');
+
+
+gulp.task('env:install', "Re-install the requirements (npm, bower, tsd, ..etc.).", function (cb) {
+  run('npm install').exec("", function () {
+    run('tsd reinstall -so').exec("", function () {
+      run('bower install').exec("", cb);
+    });
+  });
+});
