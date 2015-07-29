4489b56f36ac05235b947357ff928d04c078875d
Val Neekman <val@neekware.com>
2015-07-28 21:51:08 -0400

    add gulp-utils

--- toolbox/gulp/tasks/clean.js
+++ toolbox/gulp/tasks/clean.js
@@ -6,14 +6,14 @@ var del = require('del');
 var cfg = require('../config');
 
 
-gulp.task('del:all', "-- Clean all generated files.", function (done) {
+gulp.task('clean:all', "-- Clean all generated files.", function (done) {
   del(cfg.dist.baseDir, done);
 });
 
-gulp.task('del:dev', "-- Clean development specific generated files.", function (done) {
+gulp.task('clean:dev', "-- Clean development specific generated files.", function (done) {
   del(cfg.dist.dev.baseDir, done);
 });
 
-gulp.task('del:prod', "-- Clean production specific generated files.", function (done) {
+gulp.task('clean:prod', "-- Clean production specific generated files.", function (done) {
   del(cfg.dist.prod.baseDir, done);
 });
--- toolbox/gulp/utils.js
+++ toolbox/gulp/utils.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var plum = require('gulp-plumber');
+var utils = require('gulp-util');
 
 var plumber = function () {
 	// same are gulp.plumber, but will handle errors gracefully
 	return plum({
 		errorHandler: function (err) {
 			// ensure error don't break pipes
-			console.log(err);
+			utils.log(err.message);
 			this.emit('end');
 		}
 	});
