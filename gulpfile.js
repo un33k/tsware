8b99b9d5e3da1dec59dfdaf7f1d6f37584d12d4b
Val Neekman <val@neekware.com>
2015-08-04 20:55:07 -0400

    gulp help

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ var debug = require('gulp-debug');
 var loadtask = require('require-dir');
 var cfg = require('./toolbox/gulp/config');
 
-var gulp = require('gulp-help')(gulp, { description: '-- gulp [TASK] [OPTIONS...]' });
+var gulp = require('gulp-help')(gulp, { description: '-- Help: Ex: gulp [TASK] [OPTIONS...].' });
 
 loadtask(cfg.dir.gulpDir, { recurse: true });
