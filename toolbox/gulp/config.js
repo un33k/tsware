5b223d36b8936437ee4ad8285ed132712c385cfd
Val Neekman <val@neekware.com>
2015-08-06 16:37:20 -0400

    remove comments in config

--- toolbox/gulp/config.js
+++ toolbox/gulp/config.js
@@ -76,7 +76,7 @@ var TYPESCRIPT = {
 var LIB = {
   ol: [
     DIR.nodeDir + 'angular2/node_modules/traceur/bin/traceur-runtime.js',
-    DIR.nodeDir + 'es6-module-loader/dist/es6-module-loader.js',
+    DIR.nodeDir + 'es6-module-loader/dist/es6-.js',
     DIR.nodeDir + 'reflect-metadata/Reflect.js',
     DIR.nodeDir + 'systemjs/dist/system.js',
     DIR.nodeDir + 'zone.js/dist/zone.js'
@@ -90,7 +90,7 @@ var ANGULAR = {
   },
   libMeta: {
     'rx': {
-      format: 'cjs' // https://github.com/systemjs/builder/issues/123
+      format: 'cjs'
     }
   },
   appMeta: {
